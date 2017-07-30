a3e7b2d5cd72d5eb16968e885fb0de4d17738a1c
armyoftanks <romina.skayanyc@gmail.coms>
2017-07-30 18:23:07 -0400

    more js

---
+++ benefits.php
@@ -0,0 +1,15 @@
+
+ <!DOCTYPE html> <!-- BENEFITS -->
+<html lang="en">
+<?php include("header.html"); ?>
+<?php include("navigation.html"); ?>
+
+</body>
+</html>
+
+  <body>
+
+        <?php include("benefits.html"); ?>
+
+       </body>
+     </html>
